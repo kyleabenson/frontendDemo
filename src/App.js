1d557fbde9b291b7f6d2f3b03c93c914ad1d1651
Kyle Benson <kylebenson@kyles-mbp.lan>
2019-08-09 17:02:12 -0400

    Update to word cloud format

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import randomColor from 'randomcolor';
+import TagCloud from 'react-tag-cloud';
+import CloudItem from './CloudItem';
+import { isWhiteSpaceLike } from 'typescript';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+  large: {
+    fontSize: 60,
+    fontWeight: 'bold'
+  },
+  small: {
+    opacity: 0.7,
+    fontSize: 16
+  }
+};
+
+class App extends Component {
+  state = {
+    loading: false,
+    data: []
+  } 
+
+  componentDidMount() {
+    setInterval(() => {
+      this.forceUpdate();
+    }, 3000);
+    this.setState({loading: true})
+    fetch('https://api.github.com/repos/ansible/ansible/contributors')
+        .then(data => data.json())
+        .then(data => this.setState({data, loading: false}))
+  }
+
+  render() {
+    return (
+      <div className='app-outer'>
+        {this.state.loading
+            ? <div>"Hang tight, we're loading..."</div>
+            : <div className='app-inner'>
+                <h1>Thanks for attending</h1>
+                <TagCloud 
+                  className='tag-cloud'
+                  style={{
+                    fontFamily: 'Red Hat Display',
+                    fontSize: () => Math.round(Math.random() * 50) + 16,
+                    // fontSize: 30,
+                    color: 'white',
+                      // hue: 'blue'
+                    // }),
+                    padding: 5,
+                  }}>
+                  {this.state.data.map(
+                    user =>
+                    <CloudItem text={user.login} />
+                  )}  
+                </TagCloud>
+              </div>
+        }     
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/CloudItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CloudItem = (props) => (
+    <div { ...props } className="tag-item-wrapper">
+        <div>
+            { props.text }
+        </div>
+
+    </div>
+);
+
+export default CloudItem;
